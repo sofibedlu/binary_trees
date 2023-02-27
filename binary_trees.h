a18e53167271be6a897296e8906a735c31d1bb3d
sofi <bedlusofi@gmail.com>
2023-02-27 17:15:58 +0000

    intial commit

---
+++ binary_trees.h
@@ -0,0 +1,30 @@
+#ifndef BINARY_TREES
+#define BINARY_TREES
+
+#include <stddef.h>
+
+/**
+ * struct binary_trees_s - Binary tree node
+ *
+ * @n: integer stored in the node
+ * @parent: pointer to the parent node
+ * @left: pointer to the left child node
+ * @right: pointer to the right child node
+ */
+struct binary_tree_s
+{
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
+};
+
+typedef struct binary_tree_s binary_tree_t;
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+
+
+
+void binary_tree_print(const binary_tree_t *);
+
+#endif /*_BINARY_TREES_H_ */
