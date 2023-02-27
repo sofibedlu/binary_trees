6d366b0b7fd3b4a07919a1601d0d499f3ba89143
sofi <bedlusofi@gmail.com>
2023-02-27 19:02:25 +0000

    task-1

--- binary_trees.h
+++ binary_trees.h
@@ -5,7 +5,7 @@
 #include <stdlib.h>
 
 /**
- * struct binary_trees_s - Binary tree node
+ * struct binary_tree_s - Binary tree node
  *
  * @n: integer stored in the node
  * @parent: pointer to the parent node
@@ -23,7 +23,7 @@ struct binary_tree_s
 typedef struct binary_tree_s binary_tree_t;
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
-
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 
 
 void binary_tree_print(const binary_tree_t *);
