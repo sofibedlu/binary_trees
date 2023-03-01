220da6616e1a24cbf15a30344e9dcac0766326f8
sofi <bedlusofi@gmail.com>
2023-03-01 19:34:32 +0000

    update

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -11,13 +11,11 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new;
 
-	new = malloc(sizeof(binary_tree_t));
-	if (new == NULL || parent == NULL)
+	if (parent == NULL)
 		return (NULL);
-	new->n = value;
-	new->parent = parent;
-	new->left = NULL;
-	new->right = NULL;
+	new = binary_tree_node(parent, value);
+	if (new == NULL)
+		return (NULL);
 	if (parent->right != NULL)
 	{
 		new->right = parent->right;
